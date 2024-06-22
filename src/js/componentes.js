8a6ee2caa809f7a94b0850cd6b66a51f0e5dc2cc
Antonio Gomez Janus <antogo8768@gmail.com>
2024-06-21 21:55:34 -0500

    Restableciendo valores del cascaron de webpack

--- src/js/componentes.js
+++ src/js/componentes.js
@@ -1,75 +1,17 @@
-import { Todo } from "../classes";
-import { todoList } from "../index";
+import '../css/components.css';
+// import webpacklogo from '../assets/img/webpack-logo.png';
 
-// Referencias HTML:
+export const saludar = ( nombre ) => {
+    console.log('Creando etiqueta h1');
 
+    const h1 = document.createElement('h1');
+    h1.innerText = `Hola, ${ nombre }!!`;
 
-const divTodoList = document.querySelector('.todo-list');
-const txtInput = document.querySelector('.new-todo');
-const btnEliminar = document.querySelector('.clear-completed');
+    document.body.append( h1 );
 
-export const crearTodoHtml = ( todo ) => {
-
-    const htmlTodo = `
-    
-    <li class="${ (todo.completado) ? 'completed' : '' }" data-id="${ todo.id }">
-        <div class="view">
-            <input class="toggle" type="checkbox" ${ (todo.completado) ? 'checked' : '' }>
-            <label>${ todo.tarea }</label>
-            <button class="destroy"></button>
-        </div>
-        <input class="edit" value="Create a TodoMVC template">
-    </li>`;
-
-    const div = document.createElement('div');
-    div.innerHTML = htmlTodo;
-
-    divTodoList.append( div.firstElementChild );
-
-    return div.firstElementChild;
-
+    //img
+    // console.log(webpacklogo)
+    // const img = document.createElement('img');
+    // img.src = webpacklogo;
+    // document.body.append( img );    
 }
-
-//Eventos:
-txtInput.addEventListener('keyup', ( event ) => {
-
-    if( event.keyCode === 13 && txtInput.value.length > 0 ){
-
-        const nuevoTodo = new Todo(txtInput.value);
-        todoList.nuevoTodo( nuevoTodo );
-
-        crearTodoHtml( nuevoTodo );
-        txtInput.value = '';
-    }
-});
-
-divTodoList.addEventListener('click', ( event ) => {
-
-    const nombreElemento = (event.target.localName); //input, label, button
-    const todoElemento = event.target.parentElement.parentElement;
-    const todoId = todoElemento.getAttribute('data-id');
-
-    if( nombreElemento.includes('input')) {
-        todoList.marcarCompletado( todoId );
-        todoElemento.classList.toggle('completed');
-    }else if( nombreElemento.includes('button')) {
-        todoList.eliminarTodo( todoId );
-        divTodoList.removeChild( todoElemento );
-    }
-    
-})
-
-btnEliminar.addEventListener('click', () => {
-
-    todoList.eliminarCompletados();
-
-    for( let i = divTodoList.children.length-1; i >= 0; i-- ){
-
-        const elemento = divTodoList.children[i];
-
-        if( elemento.classList.contains('completed') ) {
-            divTodoList.removeChild( elemento );
-        }
-    }
-
-})
